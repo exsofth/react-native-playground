744d578fe6adcc5341d5ab5cc9d58e7254db314b
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2019-09-14 15:23:37 +0100

    Create style.js
    Use flex for layout

---
+++ style.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
+
+//Defining the style here
+const styles = StyleSheet.create(
+  {
+    blue: {
+      color: 'blue',
+      fontSize: 20,
+      fontWeight: 'bold'
+    },
+
+    red:{
+      color: 'red'
+    }
+  }
+);
+export default class FlexDimensionsBasics extends Component{
+  render(){
+    return(
+      <View style={{flex:1, flexDirection:'row'}}>
+        <View style={{flex:1, backgroundColor:'red'}}/>
+        <View style={{flex:2, backgroundColor:'yellow'}}/>
+        <View style={{flex:3, backgroundColor:'blue'}}/>
+        <View style={{flex:2, backgroundColor:'black'}}/>
+        <View style={{flex:1, backgroundColor:'green'}}/>
+      </View>
+    );
+  }
+}
